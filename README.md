08d885adeb7fe2575960d8109f5296f770743431
krharsh17 <kharsh39@gmail.com>
2020-05-27 02:01:32 +0530

    Debug added

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
-# claim-issue-probot
+<h1 align="center">Claimsy</h1>
 
-> A GitHub App built with [Probot](https://github.com/probot/probot) that A Probot app
+<span style="display:block;text-align:center">![image](https://media1.tenor.com/images/72a9ce1fe0e4b94d46684ad710b27c13/tenor.gif)</span>
+
+A GitHub App built with [Probot](https://github.com/probot/probot) that helps maintainers by automatically assigning willing contributors to issues! 🎉
+
+## How does it work?
+
+**Step 1** 👨‍💻 - A willing user comments on an issue with phrases like "Please assign me" or "I'd like to work on this"
+
+**Step 2** 🤖 - The bot will automatically recognize the comment and assign the commenter! 
+
+> ***Alternatively*** - You can try configuring the bot to recognize @mentions in comments and assign users accordingly!
+
+
+![image](https://media.tenor.com/images/618576ebcc4f6d2a12438624be77c54f/tenor.gif)
+
+## How do I get it running?
+
+- Simply go to the app listing and click the install button
+- Choose your account & repository, and install the app!
+- Optionally, you can provide a `.github/claimsy.yml` in your repo to customize some features of the app
+
+## Customizations?
+
+Here's a sample `.github/claimsy.yml` to let you know how to tell Claimsy what comments to look for - 
+
+```yml
+# Commands to allow contributors to get issues assigned to themselves
+selfAssignCommands:
+  - I'll do it
+  - May I do it
+
+# A flag value to allow anyone to assign anyone to an issue. This is currently experimental so use it at your own risk!
+allowUserAssignments: false
+
+# Commands to allow anyone to assign issues to anyone, including themselves
+assignCommands:
+  - assign this to USERNAME
+  - will be done by USERNAME
+
+# Remember to put in the term USERNAME as a placeholder for the @mentions that will be made in comments
+```
+
+Clarification:
+> To handle a comment such as 'assign this issue to @krharsh17', make sure to include 'assign this issue to USERNAME' in your `assignCommands` list. The keyword USERNAME is mandatory and to be used as is for placeholder of @mentions
+
 
 ## Setup
 
--- index.js
+++ index.js
@@ -1,19 +1,87 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
+var botName = require('./package.json').name
+const configFile = 'claimsy.yml'
+const defaultConfig = {
+  selfAssignCommands: ['assign me'],
+  assignCommands: ['assign USERNAME'],
+  reviewers: []
+}
+
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  app.on('issue_comment.created', async context => {
+    // Ignore if the comment was made by the bot
+    if (context.payload.comment.user.login === botName + '[bot]') { return }
+
+    context.log("1")
+
+    const config = await context.config(configFile, defaultConfig)
+
+    let assignToUser = {
+      assign: false,
+      user: context.payload.comment.user.login
+    }
+    context.log("2")
+
+    // Extract comment body
+    const commentBody = context.payload.comment.body.toString().toLowerCase()
+
+    // Check for self-assignments
+    config.selfAssignCommands.forEach((command) => {
+      if (commentBody.includes(command.toLowerCase())) {
+        assignToUser = {
+          assign: true,
+          user: context.payload.comment.user.login
+        }
+      }
+    })
+    
+    context.log("3")
+
+    // Check for mentions only if not self-assigned
+    if (assignToUser.assign !== true) {
+      // Check for mentioned user assignments
+      config.assignCommands.forEach((command) => {
+        command = command.toLowerCase()
+        context.log(command)
+        // eslint-disable-next-line no-useless-escape
+        if (commentBody.includes(command.replace('username', '@$').split('$')[0])) {
+          if (assignToUser.assign !== true) {
+            assignToUser = {
+              assign: true,
+              user: commentBody.split(command.split('username')[0])[1].split(' ')[0].replace('@', '')
+            }
+          }
+          context.log(assignToUser.assign)
+          context.log(assignToUser.user)
+        }
+      })
+    }
+
+    
+    context.log("4")
+    // Check for duplicate assignment
+    context.payload.issue.assignees.forEach((user) => {
+      if (assignToUser.user === user.login) {
+        assignToUser.assign = false
+      }
+    })
+    
+    context.log("5")
+
+    // Do nothing for non-assignment comments
+    if (assignToUser.assign === false) { return }
+
+    context.log("6")
+    // Choose photo
+    const img = (Math.random() * 10 % 2 === 0 ? 'https://media1.tenor.com/images/494ba2ef2a2826323f5bf82e9e357283/tenor.gif?itemid=16349021' : 'https://media1.tenor.com/images/72a9ce1fe0e4b94d46684ad710b27c13/tenor.gif')
+
+    context.log("7")
+    const newAssignees = context.issue({ assignees: assignToUser.user })
+
+    context.log("8")
+    await context.github.issues.addAssignees(newAssignees)
+    
+    context.log("9")
+    return context.github.issues.createComment(context.issue({ body: '<h3 align="center">Done! 🎉 </h3> \n\n ![image](' + img + ')' }))
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
